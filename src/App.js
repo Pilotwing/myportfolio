85a205571f6b034f9959db7d8fc293426910c372
Pilotwing <il.muniz@live.com>
2024-06-01 22:09:13 -0400

    add navbar and profile picture

--- src/App.js
+++ src/App.js
@@ -1,3 +1,48 @@
+import React from "react";
+import profilePic from "./2024-06-01 17.32.30.jpg";
+
 export default function App() {
-  return <h1>Hellow World!</h1>;
+  return (
+    <>
+      <Nav />
+      <div className="main-container">
+        <Profile />
+      </div>
+    </>
+  );
+}
+
+function Nav() {
+  return (
+    <nav className="navbar">
+      <a href="#" className="navbar-item">
+        Skills
+      </a>
+      <a href="#" className="navbar-item">
+        Experience
+      </a>
+      <a href="#" className="navbar-item">
+        About
+      </a>
+      <a href="#" className="navbar-item">
+        Contact
+      </a>
+    </nav>
+  );
+}
+
+function Profile() {
+  return (
+    <div className="profile-container">
+      <ProfilePicture />
+    </div>
+  );
+}
+
+function ProfilePicture() {
+  return (
+    <div className="profile-picture-container">
+      <img src={profilePic} className="profile-picture" alt="Profile pic" />
+    </div>
+  );
 }
